8d6e9c0622c5f8feab700e1abae7521438898536
harryouaida <97868704+harryouaida@users.noreply.github.com>
2022-02-20 11:19:20 +1100

    Adding comments to explain the code
    Adding comments to explain the code

--- Week1/second.sh
+++ Week1/second.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#This script was written by Hashem Ouaida Feb 2022
+#This is a simple script that displays the some text and exists the program with a code 0, which is handy for other purposes.
 echo "Hi there!"
 
 echo "It's good to see you $1"
